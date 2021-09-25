90b6967557ee9eb8c7e38a958e2443e0d2fcddc9
ElectroForez <vlad.tancev@gmail.com>
2021-09-25 23:55:17 +0300

    add more parametrs

--- razukrashka_karty.py
+++ razukrashka_karty.py
@@ -1,35 +1,31 @@
 import pandas as pd
 import plotly.graph_objects as go
 
+reg_pos_df = pd.read_csv('regions_pos.csv')
+reg_data_df = pd.read_csv('regions_data.csv')
 
-df = pd.read_csv('regions_pos.csv')
-df.head()
+data_column = 1
 
 fig = go.Figure(go.Scattermapbox(
-    mode = "markers",
-    lon = df['lon'],
-    lat = df['lat'],
-    text = df['name'] + ": " + (df['count']).astype(str),
-    marker = {'size': df['count'] / 5, 'color': df['count'], 'sizemode' : 'area'}))
-    
-#fig = go.Figure(go.Scattergeo(
-        #lon = df['lon'],
-        #lat = df['lat'],
-        #text = df['name'] + "\n " + (df['count']).astype(str),
-        #marker = dict(
-            #size = df['count']/20,
-            #color = df['count'],
-            #line_color='rgb(40,40,40)',
-            #line_width=0.5,S
-            #sizemode = 'area'
-        #)))
+    mode="markers",
+    lon=reg_pos_df['lon'],
+    lat=reg_pos_df['lat'],
+    text=reg_pos_df['name'] + ": " + (reg_data_df[str(data_column)]).astype(str),
+    marker={
+        'size': abs(reg_data_df[str(data_column)] / 5),
+        'color': reg_data_df[str(data_column)],
+        'sizemode': 'area',
+        'sizemin': 5,
+        'colorscale': [[0, 'rgb(25,125,0)'], [1, 'rgb(255,25,0)']]
+    }))
+
 
 fig.update_layout(
-    mapbox = {
+    mapbox={
         'style': "open-street-map",
-        'center': { 'lat': 57.21593, 'lon': 53.204843},
+        'center': {'lat': 57.21593, 'lon': 53.204843},
         'zoom': 8
-            },
-    margin = {'l':0, 'r':0, 'b':0, 't':0})
+    },
+    margin={'l': 0, 'r': 0, 'b': 0, 't': 0})
 
 fig.show()
